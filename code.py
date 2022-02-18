371a9512f8095f8fe98ef999b57dd6de5693e8be
Cole Timlin <95893912+AstroLeapStudios@users.noreply.github.com>
2022-02-18 18:53:43 -0500

    Main Commit
    Adding all files

---
+++ code.py
@@ -0,0 +1,23 @@
+import asyncio
+from colorama import init
+import fortnite_api
+from fortnite_api import GameLanguage
+from termcolor import cprint
+from pyfiglet import figlet_format
+import os
+
+api_key = '380463e95c4f1426977ac7bc1e6f431d4e728f43'
+
+os.system("title Flight Gaming Stats")
+cprint(figlet_format('Flight', font='big'),
+       'blue', attrs=['bold'])
+fn = fortnite_api.FortniteAPI()
+def stats(user):
+    print("----- Stats for player: "+ user + "-----\n")
+    print(fn.stats.fetch_by_name(user))
+
+
+while True:
+    user = input("Player Epic Games Account: ")
+    stats(user)
+    
