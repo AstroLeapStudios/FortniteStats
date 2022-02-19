e7e361caee6863f852a40ac3ad74dc9d5a532753
Cole Timlin <95893912+AstroLeapStudios@users.noreply.github.com>
2022-02-19 17:56:20 -0500

    API Key Bug Fix #1

--- code.py
+++ code.py
@@ -6,12 +6,17 @@ from termcolor import cprint
 from pyfiglet import figlet_format
 import os
 
-api_key = '380463e95c4f1426977ac7bc1e6f431d4e728f43'
 
 os.system("title Fortnite Stats")
 cprint(figlet_format('Stats', font='big'),
        'blue', attrs=['bold'])
-fn = fortnite_api.FortniteAPI()
+
+fileName = "key"
+print("Add API key in 'key.txt'. Retrieve it at https://www.fortnite-api.com")
+file = open(f"{fileName}.txt",encoding='latin-1')
+full_text = file.read()
+fn = fortnite_api.FortniteAPI(full_text)
+
 def stats(user):
     print("----- Stats for player: "+ user + "-----\n")
     print(fn.stats.fetch_by_name(user))
@@ -20,4 +25,4 @@ def stats(user):
 while True:
     user = input("Player Epic Games Account: ")
     stats(user)
-    
+    print("\n")
